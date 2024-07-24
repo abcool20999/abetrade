1e6c9f50c1d54a72cad18ee33a7fcd0d53168ae3
abcool20999 <abcool20999@gmail.com>
2024-07-24 13:39:57 +0100

    “trades”

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // components/Layout/Header.js
 
 import React from 'react';
@@ -40,3 +41,49 @@ const Header = ({ isAuthenticated }) => {
 };
 
 export default Header;
+=======
+// components/Layout/Header.js
+
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Header = ({ isAuthenticated }) => {
+  return (
+    <header className="list-group list-group-horizontal d-flex justify-content-between border-bottom-2">
+      <div className="logo list-group-item border-0">Abetrade</div>
+      <nav className='list-group-item border-0'>
+        <ul className="list-group list-group-horizontal">
+          <li className='list-group-item border-0 text-black'><Link to="/">How it works</Link></li>
+          <li className='list-group-item border-0 text-black'><Link to="/myaccounts">My Accounts</Link></li>
+          <li className='list-group-item border-0 text-black'><Link to="/getcapital">Get Capital</Link></li>
+
+        </ul>
+      </nav>
+      <nav>
+        <ul className='list-group-item border-0'>
+          {isAuthenticated ? (
+            <li className='list-group-item border-0'><Link to="/logout">Logout</Link></li>
+          ) : (
+            <div className='row'>
+              <div className='col-4'>
+                <li className='list-group-item border-0 m-1'>
+                  <Link to="/login">Login</Link>
+                </li>
+              </div>
+              <div className='col-8'>
+                <li className='list-group-item border-0 m-1 bg-black text-white border-dark rounded-5'>
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+              </div>
+            </div>
+
+          )}
+        </ul>
+      </nav>
+      <Outlet />
+    </header>
+  );
+};
+
+export default Header;
+>>>>>>> efed4b5 (“trades”)
