fe7b4e89ce718e0b17bd502e0eb925bd336b946d
abcool20999 <abcool20999@gmail.com>
2024-08-20 09:06:35 +0100

    output dir

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const env = loadEnv("production", process.cwd(), '')
-const __dirname = path.dirname(__filename, 'abetrade');
+const __dirname = path.dirname(__filename);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -16,14 +15,15 @@ export default defineConfig({
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
-    },
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-      // VITE_REACT_APP_GOOGLE_CLIENT_ID:"596693022834-oou2lgt3l76t8329mbvjo0p3ss6330ho.apps.googleusercontent.com"    
-      },
+  },
+  define: {
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'abetrade'), // Custom output directory
+  },
   server: {
     port: 8080,
     hot: true
   }
 })
-
