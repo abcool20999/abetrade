17c08b31dc5b6a9c0524cef392d927dc59c5baad
abcool20999 <abcool20999@gmail.com>
2024-08-20 07:55:08 +0100

    “files”

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-
-const __filename = fileURLToPath(import.meta.url);
-const env = loadEnv("production", process.cwd(), '')
-const __dirname = path.dirname(__filename);
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    },
-    },
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-      // VITE_REACT_APP_GOOGLE_CLIENT_ID:"596693022834-oou2lgt3l76t8329mbvjo0p3ss6330ho.apps.googleusercontent.com"    
-      },
-  server: {
-    port: 8080,
-    hot: true
-  }
-})
-=======
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
@@ -54,4 +26,4 @@ export default defineConfig({
     hot: true
   }
 })
->>>>>>> 597484b (file changes)
+
